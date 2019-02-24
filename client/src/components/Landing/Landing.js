9cc3ac8488b75cc22af5a2ec0bccfdb14b340454
isaac1104 <isaac-kwon@hotmail.com>
2019-02-23 18:48:28 -0800

    add Link to home on Landing.js

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'antd';
 
-const Landing = () => <h1>Insight</h1>;
+const Landing = () => (
+  <div>
+    <h1>Landing Page</h1>
+    <Button><Link to='/home'>To Home</Link></Button>
+  </div>
+);
 
 export default Landing;
