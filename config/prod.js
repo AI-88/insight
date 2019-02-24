eefe5bf0c0afdb0e98850d85e080298c4ddec1ed
isaac1104 <isaac-kwon@hotmail.com>
2019-02-23 19:34:35 -0800

    authenticate users with google oauth

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  stripePublicKey: process.env.STRIPE_PUBLIC_KEY,
+  mongodbURI: process.env.MONGODB_URI,
+  cookieKey: process.env.COOKIE_KEY
+}
