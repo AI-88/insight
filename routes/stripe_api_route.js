5d2a7749eb4c55259778bd879e8ab26a933de7d6
isaac1104 <isaac-kwon@hotmail.com>
2019-02-28 19:39:26 -0800

    refactor

--- routes/stripe_api_route.js
+++ routes/stripe_api_route.js
@@ -5,8 +5,6 @@ const requireAuth = require('../middlewares/requireAuth');
 module.exports = app => {
   app.get('/api/SUBSCRIPTIONS_DATA', requireAuth, async (req, res) => {
     const request = await stripe.subscriptions.list();
-    // const customers = await stripe.customers.list();
-    // const customer = await stripe.customers.retrieve(customers.data[0].id);
     res.send(request);
   });
 };
