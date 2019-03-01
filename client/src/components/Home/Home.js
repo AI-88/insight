119e8ffaa3f2ec5d469919307f6cce6b65acd604
isaac1104 <isaac-kwon@hotmail.com>
2019-02-28 19:25:42 -0800

    change api call to subscriptions & change action & reducer names

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchCustomerData } from '../../actions';
+import { Statistic } from 'antd';
+import { fetchSubscriptionsData } from '../../actions';
 
 class Home extends Component {
   componentDidMount() {
-    this.props.fetchCustomerData();
+    this.props.fetchSubscriptionsData();
   }
 
   render() {
-    console.log(this.props.customer_data);
+    console.log(this.props.subscriptions_data);
     const { data } = this.props.current_user;
     return (
       <div>
         <h1>Welcome, {data.firstName}</h1>
+        <Statistic
+          title='Active Subscriptions'
+          value={50}
+          precision={2}
+          suffix='%'
+        />
       </div>
     )
   }
 }
 
-const mapStateToProps = ({ current_user, customer_data }) => {
+const mapStateToProps = ({ current_user, subscriptions_data }) => {
   return {
     current_user,
-    customer_data
+    subscriptions_data
   };
 };
 
-export default connect(mapStateToProps, { fetchCustomerData })(Home);
+export default connect(mapStateToProps, { fetchSubscriptionsData })(Home);
